aeb967a2cb9f7e62225629b48b3bdb0607d20e62
Victor Manuel Lara Lopez <a15216359@alumnos.uady.mx>
2021-10-11 08:44:11 -0500

    Fixed endpoints

--- controllers/hospital.controller.js
+++ controllers/hospital.controller.js
@@ -66,30 +66,25 @@ exports.update = (request, response) => {
         if(!bodyhospital.isCorrect)
             response.status(400).send({ message: 'Incorrect data.' });
         
-        else hospital.update(id, bodyhospital.data, 
+        else Hospital.update(id, bodyhospital.data, 
             (error) => {  
                 if (error)  response.status(500).send(
                     { message: 'DB internal error.' });
-                else  Guardian.update(id, bodyGuardian.data,
-                    (error, result)=>{
-                        if (error)  response.status(500).send(
-                            { message: 'DB internal error.' });
-                        else  response.status(200).json(
-                            { message: 'Updated Successfully'}
-                        );
-                    }
-                )
+                else  response.status(200).json(
+                    { message: 'Updated Successfully'}
+                    );
             }
-        );
+        )
     }
 }
+
 
 
 exports.delete = (request, response) => {
     const id = request.params.id;
     if(typeof id === "undefined" || id<=0)
         response.status(400).send({ message: 'Invalid id.' });
-    
+
     else Hospital.delete(id, 
         (error) => {
             if (error)  response.status(500).send(
@@ -111,13 +106,8 @@ exports.delete = (request, response) => {
 
 const getDatahospital = (body) => {
     const data = {
-        names : body.names,
-        last_name : body.last_name,
-        second_last_name : body.second_last_name,
-        sex : body.sex,
-        birthday : body.birthday,
-        inscription_date : body.inscription_date,
-        id_hospital : body.id_hospital,
+        name : body.name,
+        city : body.city,
     }
     
     return {isCorrect: validateData(data), data: data}
@@ -128,9 +118,6 @@ const validateData = (data) =>{
     if((Object.keys(data)).some((key)=> typeof data[key] === 'undefined') ||
         (Object.keys(data)).some((key)=> data[key].length === 0))
             isCorrect = false;
-    
-    else if(data.sex && data.sex!=='M' && data.sex!=='F')
-        isCorrect = false
     
     return isCorrect;
 }
