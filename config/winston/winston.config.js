dbd9dc7dd9d477a729a6dbbc2d1312a5b14275ec
Jef Peniche <jeff@nativodigital.com.mx>
2021-10-20 16:19:51 -0500

    update log format

--- config/winston/winston.config.js
+++ config/winston/winston.config.js
@@ -1,16 +1,39 @@
-const { createLogger, format, transports, config, error } = require("winston");
-const { combine, timestamp, json, errors, printf } = format;
+const winston = require("winston");
+const DailyRotateFile = require("winston-daily-rotate-file");
 const path = require("path");
 
-const myFormat = printf(({ level, message, label, timestamp }) => {
-    return `[${timestamp}] ${level}: ${message} \n`;
+const myFormat = winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    winston.format.json(),
+    winston.format.errors({ stack: true }),
+    winston.format.align(),
+    winston.format.printf((info) => `${info.timestamp} ${info.level.toUpperCase()}: ${info.message}`)
+);
+
+const transport = new DailyRotateFile({
+    filename: "application-%DATE%.log",
+    dirname: path.join(__dirname, "../../", "logs"),
+    datePattern: "YYYY-MM-DD-HH",
+    zippedArchive: true,
+    maxSize: "20m",
+    maxFiles: "14d",
+    prepend: true,
+    level: "debug",
 });
 
+const logger = winston.createLogger({
+    format: myFormat,
+    transports: [transport, new winston.transports.Console({ level: "info" })],
+});
+
+/*
 const logger = createLogger({
     level: "debug",
     format: combine(timestamp(), json(), errors({ stack: true }), myFormat),
-    transports: [new transports.Console(), new transports.File({ maxsize: 104857600, dirname: path.join(__dirname, "../../", "logs") })],
+    transports: [new transports.Console(), new transports.File({ filename: "application-%DATE%.log", rotationFormat: true, maxFiles: 14, maxsize: 104857600, dirname: path.join(__dirname, "../../", "logs") })],
 });
+*/
 
 loggerDefaultRoute = (req, res, next) => {
     const { method, url, params, headers } = req;
